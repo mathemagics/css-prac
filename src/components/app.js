c94ca588ddf61a386fca459e12a9c11c30330f64
Paul WB <higgsbison@gmail.com>
2017-02-22 15:21:39 -0800

    add header component

--- src/components/app.js
+++ src/components/app.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import Header from './Header';
 
 export default (props) => {
   return (
-    <div className="app">{props.children}</div>
+    <div className="app">
+      <Header />
+      {props.children}
+    </div>
   );
 };
