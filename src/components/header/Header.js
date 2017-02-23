3f6921454a79679f214748cdc7436ed2d132c4a4
Paul WB <higgsbison@gmail.com>
2017-02-22 16:40:25 -0800

    initial style header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -6,13 +6,13 @@ class Header extends Component {
   render() {
     return (
       <div className="header">
-        <ul className="">
+        <ul className="header__link-list">
           <Link className="header__link" to="/shop">Shop</Link>
           <Link className="header__link" to="/adventures">Adventures</Link>
           <Link className="header__link" to="/about">About</Link>
         </ul>
-        <div>logo</div>
-        <ul>
+        <Link className="header__link header__link--title" to="/">Abbott</Link>
+        <ul className="header__link-list">
           <Link className="header__link" to="signup">join</Link>
           <Link className="header__link" to="signin">Login</Link>
         </ul>
