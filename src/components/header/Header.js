5a9d6679153252ae8daa6387784813ce86ae5522
Paul WB <higgsbison@gmail.com>
2017-02-22 17:21:37 -0800

    add scroll detection to header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -3,9 +3,23 @@ import { Link } from 'react-router';
 import './styles.scss';
 
 class Header extends Component {
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll.bind(this));
+  }
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.handleScroll);
+  }
+  handleScroll() {
+    if (this.header !== null) {
+      if (window.scrollY > 30) {
+        this.header.style.backgroundColor = 'red';
+      }
+    }
+  }
+
   render() {
     return (
-      <div className="header">
+      <div className="header" ref={(header) => { this.header = header; }}>
         <ul className="header__link-list">
           <Link className="header__link" to="/shop">Shop</Link>
           <Link className="header__link" to="/adventures">Adventures</Link>
