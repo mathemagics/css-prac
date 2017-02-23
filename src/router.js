51b36a959776e17e2829a0141f90942ff5da8071
Benjamin Brenner <ben.l.brenner@gmail.com>
2017-02-22 18:16:05 -0800

    Created jumbotron skeleton and data file, as well as minor css changes

--- src/router.js
+++ src/router.js
@@ -3,15 +3,11 @@ import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 import App from './components/app';
 import Main from './components/main';
 
-import Product from './components/product';
-
-
 export default () => {
   return (
     <Router history={browserHistory}>
       <Route path="/" component={App}>
         <IndexRoute component={Main} />
-        <IndexRoute component={Product} />
       </Route>
     </Router>
   );
