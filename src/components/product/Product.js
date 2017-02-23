6390b3eae56d228b5e55a932ea9606e1ca1ebf27
Benjamin Brenner <ben.l.brenner@gmail.com>
2017-02-23 12:42:42 -0800

    Adjusted styling for product links

--- src/components/product/Product.js
+++ src/components/product/Product.js
@@ -23,20 +23,40 @@ class Product extends Component {
         <div className="product">
           <img src={blue} alt="blue" className="product__primary-image" />
           <img src={blue2} alt="blue2" className="product__secondary-image" />
-          <ProductLinks color={'blue'} />
+          <ProductLinks
+            color={'blue'}
+            name={'THE CAPE'}
+            coords={'41.6787° N , 70.4789° W'}
+            description={'MINT - GINGER - MOSS'}
+          />
         </div>
         <div className="product">
-          <ProductLinks color={'green'} />
+          <ProductLinks
+            color={'green'}
+            name={'SEQUOIA'}
+            coords={'36.4864° N , 1 1 8.5658° W'}
+            description={'SMOKEY INCENSE - CEDARWOOD - SANDALWOOD'}
+          />
           <img src={green1} alt="Test" className="product__primary-image" />
           <img src={green2} alt="Test2" className="product__secondary-image" />
         </div>
         <div className="product">
           <img src={brown1} alt="Test" className="product__primary-image" />
           <img src={brown2} alt="Test2" className="product__secondary-image" />
-          <ProductLinks color={'brown'} />
+          <ProductLinks
+            color={'brown'}
+            name={'MOJAVE'}
+            coords={'35.4166° N , 115.5842° W'}
+            description={'BERGAMOT - BLACK PEPPER - TOBACCO LEAF'}
+          />
         </div>
         <div className="product">
-          <ProductLinks color={'red'} />
+          <ProductLinks
+            color={'red'}
+            name={'TELLURIDE'}
+            coords={'37.9375° N , 107.8123° W'}
+            description={'SUEDE - SANDALWOOD - TONKA BEAN'}
+          />
           <img src={red1} alt="Test" className="product__primary-image" />
           <img src={red2} alt="Test2" className="product__secondary-image" />
         </div>
--- src/components/productLinks/ProductLinks.js
+++ src/components/productLinks/ProductLinks.js
@@ -5,21 +5,23 @@ import './styles.scss';
 export default (props) => {
   return (
     <div className="product-links">
-      <div className="product-links__text">
-        <p className="product-links__text--name">THE CAPE</p>
-        <p className="product-links__text--coords">41.6787° N , 70.4789° W</p>
-        <p className="product-links__text--description">MINT - GINGER - MOSS</p>
+      <div className="product-links__aligner">
+        <div className="product-links__text">
+          <p className="product-links__text--name">{props.name}</p>
+          <p className="product-links__text--coords">{props.coords}</p>
+          <p className="product-links__text--description">{props.description}</p>
+        </div>
+        <button
+          className={`product-links__button product-links__button--primary-${props.color}`}
+        >
+          shop now
+        </button>
+        <button
+          className={`product-links__button product-links__button--secondary-${props.color}`}
+        >
+          see the adventure
+        </button>
       </div>
-      <button
-        className={`product-links__button product-links__button--primary-${props.color}`}
-      >
-        shop now
-      </button>
-      <button
-        className={`product-links__button product-links__button--secondary-${props.color}`}
-      >
-        see the adventure
-      </button>
     </div>
   );
 };
