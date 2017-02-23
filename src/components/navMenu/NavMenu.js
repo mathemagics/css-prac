71506e8335bdc4f629c97faf702483808df610df
Paul WB <higgsbison@gmail.com>
2017-02-23 13:01:57 -0800

    add navmenu component

---
+++ src/components/navMenu/NavMenu.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class NavMenu extends Component {
+  render() {
+    return (
+      <div className="nav-menu">
+        NavMenu
+      </div>
+    );
+  }
+}
+
+export default NavMenu;
