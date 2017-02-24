56f541942353b5809ac8bcff0da55596b5200b09
Paul WB <higgsbison@gmail.com>
2017-02-24 15:44:11 -0800

    add shop component

---
+++ src/components/shop/Shop.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+import './styles.scss';
+
+class Shop extends Component {
+  render() {
+    return <div>shop</div>
+  }
+}
+
+export default Shop;
--- src/router.js
+++ src/router.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 import App from './components/app';
 import Main from './components/main';
+import Shop from './components/shop';
 
 export default () => {
   return (
     <Router history={browserHistory}>
       <Route path="/" component={App}>
         <IndexRoute component={Main} />
+        <Route path="/shop" component={Shop} />
       </Route>
     </Router>
   );
