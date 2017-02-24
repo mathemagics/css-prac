da581323c504f156b65e5e2ff6709e7012574ed1
Benjamin Brenner <ben.l.brenner@gmail.com>
2017-02-23 18:53:02 -0800

    Implemented fade for jumbotron

--- src/components/jumbotron/Jumbotron.js
+++ src/components/jumbotron/Jumbotron.js
@@ -12,20 +12,18 @@ import image5 from '../../assets/images/jumbotron5.jpg';
 const imageArray = [image1, image2, image3, image4, image5];
 let count = 0;
 
-
 class Jumbotron extends Component {
-  constructor(props) {
-    super(props);
+  constructor() {
+    super();
 
     this.state = { currentImage: imageArray[0] };
   }
 
   componentDidMount() {
-    this.timer = setInterval(this.renderImage.bind(this), 7000);
-  }
-  renderImage() {
-    count = count < imageArray.length - 1 ? count += 1 : 0;
-    this.setState({ currentImage: imageArray[count] });
+    window.setInterval(() => {
+      count = count < imageArray.length - 1 ? count += 1 : 0;
+      this.setState({ currentImage: imageArray[count] });
+    }, 6000);
   }
 
   render() {
@@ -36,10 +34,21 @@ class Jumbotron extends Component {
         transitionEnterTimeout={400}
         transitionLeaveTimeout={400}
       >
-        <div className="jumbotron">
-          <h1 className="jumbotron__header">Scents for an Inspired Life</h1>
-          <Link to="/shop" className="jumbotron__link">SHOP NOW</Link>
-          <img src={this.state.currentImage} className="jumbotron__image" />
+        <div className="slide__container">
+          <ReactCSSTransitionGroup
+            transitionEnterTimeout={3000}
+            transitionLeaveTimeout={3000}
+            transitionName="slide"
+          >
+            <div
+              className="jumbotron"
+              key={this.state.currentImage}
+            >
+              <h1 className="jumbotron__header">Scents for an Inspired Life</h1>
+              <Link to="/shop" className="jumbotron__link">SHOP NOW</Link>
+              <img src={this.state.currentImage} className="jumbotron__image"/>
+            </div>
+          </ReactCSSTransitionGroup>
         </div>
       </ReactCSSTransitionGroup>
     );
