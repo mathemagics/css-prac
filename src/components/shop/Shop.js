9bdfa7ed1afaa961d65c1e82b087d7b4ceaf5b26
Paul WB <higgsbison@gmail.com>
2017-02-24 16:22:10 -0800

    add shopitems to shop

--- src/components/shop/Shop.js
+++ src/components/shop/Shop.js
@@ -1,10 +1,28 @@
 import React, { Component } from 'react';
 
+import ShopItem from '../shopItem';
+
+import blue from '../../assets/images/blue2.png';
+import green from '../../assets/images/green2.png';
+import brown from '../../assets/images/brown2.png';
+import red from '../../assets/images/red2.png';
+
 import './styles.scss';
 
 class Shop extends Component {
   render() {
-    return <div>shop</div>
+    return (
+      <div className="shop">
+        <div className="shop__row">
+          <ShopItem img={blue} />
+          <ShopItem img={green} />
+        </div>
+        <div className="shop__row">
+          <ShopItem img={brown} />
+          <ShopItem img={red} />
+        </div>
+      </div>
+    );
   }
 }
 
