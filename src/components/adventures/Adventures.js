fa704e3e65f045a3e1ddfccaf8c2076169def107
Paul WB <higgsbison@gmail.com>
2017-02-24 17:25:28 -0800

    build adventures page

--- src/components/adventures/Adventures.js
+++ src/components/adventures/Adventures.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
+import cape from '../../assets/images/adventures-cape.jpg';
+import mojave from '../../assets/images/adventures-mojave.jpg';
+import sequoia from '../../assets/images/adventures-sequoia.jpg';
+import telluride from '../../assets/images/adventures-telluride.jpg';
 
 import './styles.scss';
 
 class Adventures extends Component {
   render() {
     return (
-      <div> Adventures </div>
+      <div className="adventures">
+        <h2>Explore the places that inspired our scents.</h2>
+        <img src={cape} alt="" className="adventures__img" />
+        <img src={mojave} alt="" className="adventures__img" />
+        <img src={sequoia} alt="" className="adventures__img" />
+        <img src={telluride} alt="" className="adventures__img" />
+      </div>
     );
   }
 }
--- src/router.js
+++ src/router.js
@@ -3,12 +3,13 @@ import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 import App from './components/app';
 import Main from './components/main';
 import Shop from './components/shop';
+import Adventures from './components/adventures';
 
 export default () => {
   return (
     <Router history={browserHistory}>
       <Route path="/" component={App}>
-        <IndexRoute component={Main} />
+        <IndexRoute component={Adventures} />
         <Route path="/shop" component={Shop} />
       </Route>
     </Router>
