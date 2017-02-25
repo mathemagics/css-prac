e485a78259fc475784b7ed733d885fb06d13d05a
Paul WB <higgsbison@gmail.com>
2017-02-24 17:03:03 -0800

    add adventures

---
+++ src/components/adventures/Adventures.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+import './styles.scss';
+
+class Adventures extends Component {
+  render() {
+    return (
+      <div> Adventures </div>
+    );
+  }
+}
+
+export default Adventures;
