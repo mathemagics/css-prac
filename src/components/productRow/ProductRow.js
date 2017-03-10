504f0df498e3ef3add694718fc7811ef3ea9bfbe
Benjamin Brenner <ben.l.brenner@gmail.com>
2017-03-09 21:11:09 -0800

    Added lazy-loading for primary image

--- src/components/productRow/ProductRow.js
+++ src/components/productRow/ProductRow.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import LazyLoad from 'react-lazyload';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 
 import ProductLinks from '../productLinks';
 import './styles.scss';
@@ -6,7 +8,18 @@ import './styles.scss';
 export default (props) => {
   return (
     <div className="product-row">
-      <img src={props.primaryImage} className="products__primary-image" />
+      <LazyLoad throttle={200} height={300} scroll>
+        <ReactCSSTransitionGroup
+          key="1"
+          transitionName="fade"
+          transitionAppear={true}
+          transitionAppearTimeout={500}
+          transitionEnter={false}
+          transitionLeave={false}
+        >
+          <img src={props.primaryImage} className="products__primary-image" />
+        </ReactCSSTransitionGroup>
+      </LazyLoad>
       <img src={props.secondaryImage} className="products__secondary-image" />
       <ProductLinks
         name={props.name}
